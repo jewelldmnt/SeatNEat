bec73bd0eeed6f6c40a46c666563324df6d13cd6
Jewell Anne Diamante <jewellanneddiamante@iskolarngbayan.pup.edu.ph>
2025-02-18 15:28:31 +0800

    fix: updated the button

--- frontend/src/components/UI/Button.jsx
+++ frontend/src/components/UI/Button.jsx
@@ -11,21 +11,21 @@ const Button = ({
   // Define size styles
   const sizeClasses = {
     sm: "p-[0.62rem] h-[2.375rem] text-label-lg font-semibold",
-    md: "px-[1.75rem] py-[1.25rem] text-base",
+    md: "px-[1.75rem] py-[1.12rem] text-base",
     lg: "px-6 py-3 text-lg",
   };
 
   // Define variant styles
   const variantClasses = {
-    default: "bg-accent text-black",
-    filled: "bg-neutral-300 text-black",
-    outlined: "bg-transparent text-primary border border-accent",
+    default: "bg-accent",
+    filled: "bg-neutral-300",
+    outlined: "bg-transparent border border-netural-400",
   };
 
   return (
     <button
       className={clsx(
-        "transition duration-200 hover:bg-secondary",
+        "text-black font-semibold text-label-lg transition duration-200 hover:bg-secondary",
         sizeClasses[size],
         variantClasses[variant],
         className
