6fa55b0bf78e3afb6f2229c3841543247bf402a1
Jewell Anne Diamante <jewellanneddiamante@iskolarngbayan.pup.edu.ph>
2025-02-24 09:04:03 +0800

    fix: fix the sticky navbar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Cusine from '../components/Layout/Home/Cusine'
-import Popular from '../components/Layout/Home/Popular'
-import Location from '../components/Layout/Home/Location'
-import Deals from '../components/Layout/Home/Deals'
+import React from "react";
+import Cusine from "../components/Layout/Home/Cusine";
+import Popular from "../components/Layout/Home/Popular";
+import Location from "../components/Layout/Home/Location";
+import Deals from "../components/Layout/Home/Deals";
 
 const Home = () => {
   return (
-    <div>
-        <Cusine />
-        <Popular />
-        <Location />
-        <Deals />
+    <div className="pt-[4rem] min-h-[calc(100vh-4rem)]">
+      <Cusine />
+      <Popular />
+      <Location />
+      <Deals />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
